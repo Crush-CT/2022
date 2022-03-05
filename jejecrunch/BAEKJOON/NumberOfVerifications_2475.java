ae23d59babf2ca13cb3f7823f0c959de3a883d1f
jejecrunch <gkswlgp456@gmail.com>
2022-03-06 03:09:08 +0900

    add: 2475 검증수

---
+++ jejecrunch/BAEKJOON/NumberOfVerifications_2475.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class NumberOfVerifications_2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] num = br.readLine().split(" ");
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            sum += Math.pow(num[i].charAt(0) - '0', 2);
+        }
+        sum %= 10;
+        System.out.println(sum);
+    }
+}
