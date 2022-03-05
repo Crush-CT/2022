6920922d412f04a7365f33dafd7649dd10361e4d
jejecrunch <gkswlgp456@gmail.com>
2022-03-06 03:04:07 +0900

    add: 24082 立方体

---
+++ jejecrunch/BAEKJOON/Riltupoutai_24082.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Riltupoutai_24082 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.pow(X, 3));
+    }
+}
