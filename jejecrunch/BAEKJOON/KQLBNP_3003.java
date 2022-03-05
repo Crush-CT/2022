97c625719ac05f0708c610eeaa67aa679327a34e
jejecrunch <gkswlgp456@gmail.com>
2022-03-06 03:02:38 +0900

    add: 3003 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ jejecrunch/BAEKJOON/KQLBNP_3003.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class KQLBNP_3003 {
+    public static void main(String[] args) throws IOException {
+        int[] stand = new int[] { 1, 1, 2, 2, 2, 8 };
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        for (int i = 0; i < line.length; i++) {
+            System.out.print(stand[i] - Integer.parseInt(line[i]) + " ");
+        }
+    }
+}
