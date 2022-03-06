cfea4b45866861c554e7e74ca381c6005baad3bb
jejecrunch <gkswlgp456@gmail.com>
2022-03-06 23:58:57 +0900

    add: 4299 AFC 윔블던

---
+++ jejecrunch/BAEKJOON/AFCWhimbledon_4299.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class AFCWhimbledon_4299 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int TOTAL = Integer.parseInt(str[0]);
+        int MINUS = Integer.parseInt(str[1]);
+        int stand = TOTAL - MINUS;
+
+        if (stand < 0) {
+            System.out.println(-1);
+        } else {
+            if (stand % 2 == 0) {
+                System.out.println((stand / 2 + MINUS) + " " + (stand / 2));
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
