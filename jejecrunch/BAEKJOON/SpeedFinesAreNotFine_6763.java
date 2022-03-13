d6fc95e84cb58787143f0320fc888bf8af595ff6
jejecrunch <gkswlgp456@gmail.com>
2022-03-13 23:28:09 +0900

    add: 6763 Speed fines are not fine!

---
+++ jejecrunch/BAEKJOON/SpeedFinesAreNotFine_6763.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class SpeedFinesAreNotFine_6763 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int limit = Integer.parseInt(br.readLine());
+        int speed = Integer.parseInt(br.readLine());
+
+        if (speed - limit <= 0) {
+            System.out.println("Congratulations, you are within the speed limit!");
+        } else if (speed - limit >= 1 && speed - limit <= 20) {
+            System.out.println("You are speeding and your fine is $100.");
+        } else if (speed - limit >= 21 && speed - limit <= 30) {
+            System.out.println("You are speeding and your fine is $270.");
+        } else if (speed - limit >= 31) {
+            System.out.println("You are speeding and your fine is $500.");
+        }
+    }
+}
