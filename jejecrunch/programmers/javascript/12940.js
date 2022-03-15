3073bc6ea14395809c12cc4fb7375c2ae5ffdf8e
jejecrunch <gkswlgp456@gmail.com>
2022-03-15 22:46:04 +0900

    add: 12940 최대공약수와 최소공배수

---
+++ jejecrunch/programmers/javascript/12940.js
@@ -0,0 +1,14 @@
+function solution(n, m) {
+    var answer = [gcd(n, m), lcm(n, m)]
+    return answer
+}
+
+function gcd(a, b) {
+    if (b == 0) return a
+
+    return gcd(b, a % b)
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b)
+}
