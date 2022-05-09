00a7ad3037250a59d7baa2c5a499ebe2e4034003
jejecrunch <gkswlgp456@gmail.com>
2022-05-09 22:57:21 +0900

    add: 1100 하얀 칸

--- jejecrunch/BAEKJOON/javascript/a220508/20944.js
+++ jejecrunch/BAEKJOON/javascript/a220508/20944.js
@@ -5,10 +5,30 @@ const rl = readline.createInterface({
     output: process.stdout,
 })
 
+function solve(input) {
+    let result = 0
+    for (let i = 0; i < input.length; i++) {
+        if (i % 2 == 0) {
+            for (let j = 0; j < input[i].length; j++) {
+                if (j % 2 == 0) if (input[i][j] == "F") result++
+            }
+        } else {
+            for (let j = 0; j < input[i].length; j++) {
+                if (j % 2 == 1) if (input[i][j] == "F") result++
+            }
+        }
+    }
+    console.log(result)
+}
+
+let input = []
+let count = 0
+
 rl.on("line", (line) => {
-    const arr = new Array(+line).fill("a")
-    console.log(arr.join(""))
-    rl.close()
+    if (count < 9) input.push(line.split(""))
+    else rl.close()
+    count++
 }).on("close", () => {
+    solve(input)
     process.exit()
 })
