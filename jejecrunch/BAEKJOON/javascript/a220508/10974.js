8f446329b99d4ba3edd6936e5fad14f6853bb5e6
제제jeje <gkswlgp456@gmail.com>
2022-09-28 03:22:33 +0900

    add: 10974 모든 순열

---
+++ jejecrunch/BAEKJOON/javascript/a220508/10974.js
@@ -0,0 +1,45 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+let count = 0
+let N = 0
+
+function getPermutations(arr, selectNumber) {
+    const results = []
+    if (selectNumber === 1) return arr.map((el) => [el])
+    // n개중에서 1개 선택할 때(nP1), 바로 모든 배열의 원소 return. 1개선택이므로 순서가 의미없음.
+
+    arr.forEach((fixed, index, origin) => {
+        const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]
+        // 해당하는 fixed를 제외한 나머지 배열
+        const permutations = getPermutations(rest, selectNumber - 1)
+        // 나머지에 대해서 순열을 구한다.
+        const attached = permutations.map((el) => [fixed, ...el])
+        //  돌아온 순열에 떼 놓은(fixed) 값 붙이기
+        results.push(...attached)
+        // 배열 spread syntax 로 모두다 push
+    })
+
+    return results // 결과 담긴 results return
+}
+
+function solve(N) {
+    const arr = []
+    for (let i = 0; i < N; i++) {
+        arr.push(i + 1)
+    }
+
+    const res = getPermutations(arr, N).map((v) => v.join(" "))
+    console.log(res.join("\n"))
+}
+
+rl.on("line", (line) => {
+    N = +line
+}).on("close", () => {
+    solve(N)
+    process.exit()
+})
